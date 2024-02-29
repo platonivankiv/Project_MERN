571a1651eba1dbf328d005b82761e7178c6ffaf5
platon.ivankiv <ivankiv.platon@gmail.com>
2024-02-29 16:47:52 +0300

    Create CRUD

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import {registerValidation} from "./validations/auth.js";
-import {validationResult} from 'express-validator'
-import UserModel from "./models/User.js"
+import {registerValidation, loginValidation, postCreateValidation} from "./validation.js";
+import checkAuth from "./utils/checkAuth.js"
+import * as UserController from "./controllers/UserController.js"
+import * as PostController from "./controllers/PostController.js"
+
 
 
 mongoose
@@ -16,28 +17,19 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
-    }
-
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    })
-
-    const user = await doc.save()
+app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/login', loginValidation, UserController.login)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 
-    res.json(user)
-})
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts',checkAuth, postCreateValidation, PostController.create)
+app.delete('/posts/:id',checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, PostController.update)
+
+
+
 
 app.listen(4444, (err) => {
     if (err) {
