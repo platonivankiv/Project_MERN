706705a1cacd9409f11090a4feede569150b8cfb
platon.ivankiv <ivankiv.platon@gmail.com>
2024-03-27 12:04:19 +0300

    update PostController

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,131 +1,128 @@
-import PostModel from "../models/Post.js";
-
+import PostModel from '../models/Post.js'
 
 export const getLastTags = async (req, res) => {
-    try {
-        const posts = await PostModel.find().limit(5).exec()
+	try {
+		const posts = await PostModel.find().limit(5).exec()
 
-        const tags = posts
-            .map(obj => obj.tags)
-            .flat()
-            .slice(0, 5)
+		const tags = posts
+			.map(obj => obj.tags)
+			.flat()
+			.slice(0, 5)
 
-        res.json(tags)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось получить теги'
-        })
-    }
+		res.json(tags)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось получить теги',
+		})
+	}
 }
 export const getAll = async (req, res) => {
-    try {
-        const posts = await PostModel.find().populate('user').exec()
-        res.json(posts)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось получить посты'
-        })
-    }
+	try {
+		const posts = await PostModel.find().populate('user').exec()
+		res.json(posts)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось получить посты',
+		})
+	}
 }
 export const getOne = async (req, res) => {
-    try {
-        const postId = req.params.id
+	try {
+		const postId = req.params.id
 
-        PostModel.findOneAndUpdate(
-            {
-                _id: postId,
-            },
-            {
-                $inc: {viewsCount: 1}
-            },
-            {
-                returnDocument: 'after'
-            }
-        ).then((post) => {
-            if (!post) {
-                return res.status(404).json({
-                    message: 'Пост не найден'
-                })
-            }
-            res.json(post)
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось получить посты'
-        })
-    }
+		PostModel.findOneAndUpdate(
+			{
+				_id: postId,
+			},
+			{
+				$inc: { viewsCount: 1 },
+			},
+			{
+				returnDocument: 'after',
+			}
+		).then(post => {
+			if (!post) {
+				return res.status(404).json({
+					message: 'Пост не найден',
+				})
+			}
+			res.json(post)
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось получить посты',
+		})
+	}
 }
 export const create = async (req, res) => {
-    try {
-        const doc = new PostModel({
-            title: req.body.title,
-            text: req.body.text,
-            tags: req.body.tags,
-            imageUrl: req.body.imageUrl,
-            user: req.userId,
-        })
+	try {
+		const doc = new PostModel({
+			title: req.body.title,
+			text: req.body.text,
+			tags: req.body.tags.split(','),
+			imageUrl: req.body.imageUrl,
+			user: req.userId,
+		})
 
-        const post = await doc.save()
+		const post = await doc.save()
 
-        res.json(post)
-
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось создать пост'
-        })
-    }
+		res.json(post)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось создать пост',
+		})
+	}
 }
 export const remove = async (req, res) => {
-    try {
-        const postId = req.params.id
+	try {
+		const postId = req.params.id
 
-        PostModel.findOneAndDelete(
-            {
-                _id: postId,
-            }).then((post) => {
-            if (!post) {
-                return res.status(404).json({
-                    message: 'Пост не найден'
-                })
-            }
-            res.json({
-                success: true
-            })
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось удалить пост'
-        })
-    }
+		PostModel.findOneAndDelete({
+			_id: postId,
+		}).then(post => {
+			if (!post) {
+				return res.status(404).json({
+					message: 'Пост не найден',
+				})
+			}
+			res.json({
+				success: true,
+			})
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось удалить пост',
+		})
+	}
 }
 export const update = async (req, res) => {
-    try {
-        const postId = req.params.id
+	try {
+		const postId = req.params.id
 
-        await PostModel.findOneAndUpdate(
-            {
-                _id: postId
-            },
-            {
-                title: req.body.title,
-                text: req.body.text,
-                tags: req.body.tags,
-                imageUrl: req.body.imageUrl,
-                user: req.userId,
-            })
-        res.json({
-            success: true
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось обновить пост'
-        })
-    }
+		await PostModel.findOneAndUpdate(
+			{
+				_id: postId,
+			},
+			{
+				title: req.body.title,
+				text: req.body.text,
+				tags: req.body.tags.split(','),
+				imageUrl: req.body.imageUrl,
+				user: req.userId,
+			}
+		)
+		res.json({
+			success: true,
+		})
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Не удалось обновить пост',
+		})
+	}
 }
--- validation.js
+++ validation.js
@@ -1,22 +1,24 @@
-import {body} from 'express-validator'
+import { body } from 'express-validator'
 
 export const loginValidation = [
-    body('email', 'Неверный формат почты').isEmail(),
-    body('password', 'Пароль должен быть минимум 5 символов').isLength({min: 5}),
+	body('email', 'Неверный формат почты').isEmail(),
+	body('password', 'Пароль должен быть минимум 5 символов').isLength({
+		min: 5,
+	}),
 ]
 
 export const registerValidation = [
-    body('email', 'Неверный формат почты').isEmail(),
-    body('password', 'Пароль должен быть минимум 5 символов').isLength({min: 5}),
-    body('fullName', 'Укажите имя').isLength({min: 3}),
-    body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
+	body('email', 'Неверный формат почты').isEmail(),
+	body('password', 'Пароль должен быть минимум 5 символов').isLength({
+		min: 5,
+	}),
+	body('fullName', 'Укажите имя').isLength({ min: 3 }),
+	body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ]
 
-export const postCreateValidation  = [
-    body('title', 'Введите заголовок поста').isLength({min: 3}).isString(),
-    body('text', 'Введите текст поста').isLength({min: 3}).isString(),
-    body('tags', 'Неверный формат тегов').optional().isString(),
-    body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
-
+export const postCreateValidation = [
+	body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
+	body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
+	body('tags', 'Неверный формат тэгов').optional().isString(),
+	body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
 ]
-
